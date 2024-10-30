79997558d9c335a524a9f93368667cd80ddd44da
Alex <a_pinot@icloud.com>
2024-10-30 16:58:47 +0100

    removing local assets

--- composables/usePaintingsStore.ts
+++ composables/usePaintingsStore.ts
@@ -1,52 +1,52 @@
-import { useState } from "nuxt/app";
-import { Ref } from "vue";
+// import { useState } from "nuxt/app";
+// import { Ref } from "vue";
 
-export interface Picture {
-  url: string;
-  alternativeText: string;
-}
+// export interface Picture {
+//   url: string;
+//   alternativeText: string;
+// }
 
-export interface Painting {
-  id: number;
-  slug: string;
-  name: string;
-  cursor: string;
-  date: string;
-  type: string;
-  width: number;
-  length: number;
-  depth: number;
-  support: string;
-  picture: Picture;
-  pictures: Picture[];
-}
+// export interface Painting {
+//   id: number;
+//   slug: string;
+//   name: string;
+//   cursor: string;
+//   date: string;
+//   type: string;
+//   width: number;
+//   length: number;
+//   depth: number;
+//   support: string;
+//   picture: Picture;
+//   pictures: Picture[];
+// }
 
-export const usePaintingsStore = () => {
-  const paintings: Ref<Painting[]> = useState(
-    "paintings",
-    () => paintings || []
-  );
+// export const usePaintingsStore = () => {
+//   const paintings: Ref<Painting[]> = useState(
+//     "paintings",
+//     () => paintings || []
+//   );
 
-  const getPaintingByTitle = (
-    name: string
-  ): { index: number; painting: Painting } => {
-    const index = paintings.value.findIndex(
-      (painting) => painting.name === name
-    );
+//   const getPaintingByTitle = (
+//     name: string
+//   ): { index: number; painting: Painting } => {
+//     const index = paintings.value.findIndex(
+//       (painting) => painting.name === name
+//     );
 
-    return {
-      index,
-      painting: paintings.value[index],
-    };
-  };
+//     return {
+//       index,
+//       painting: paintings.value[index],
+//     };
+//   };
 
-  const getNextPainting = (index: number): Painting => {
-    return paintings.value[(index + 1) % paintings.value.length];
-  };
+//   const getNextPainting = (index: number): Painting => {
+//     return paintings.value[(index + 1) % paintings.value.length];
+//   };
 
-  return {
-    paintings,
-    getPaintingByTitle,
-    getNextPainting,
-  };
-};
+//   return {
+//     paintings,
+//     getPaintingByTitle,
+//     getNextPainting,
+//   };
+// };
