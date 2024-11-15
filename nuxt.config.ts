07c96c5ab1297f957b1f9a54fe001e439763e612
Alex <a_pinot@icloud.com>
2024-11-15 17:52:35 +0100

    static setup && dep updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,7 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  nitro: {
-    static: true,
-  },
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
   sourcemap: {
     server: false,
     client: false,
